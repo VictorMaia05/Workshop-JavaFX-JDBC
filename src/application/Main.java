5a3c30410d692fe6803f50c85d546cfbe3c37948
Victor Maia <Victor@MacBook-Pro-de-Douglas.local>
2021-08-18 15:21:19 -0300

    Department

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) { 
 		try {
@@ -17,8 +20,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
-			mainScene.getRoot().setStyle("-fx-font-family: Arial");
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -26,6 +28,10 @@ public class Main extends Application {
 		catch (IOException e) {
 			e.printStackTrace();
 		} 
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	
 	public static void main(String[] args) {
