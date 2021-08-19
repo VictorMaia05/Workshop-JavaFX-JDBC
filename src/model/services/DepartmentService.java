317e4c559e7e033d851919dcec54cfd84b97e8ce
Victor Maia <Victor@MacBook-Pro-de-Douglas.local>
2021-08-19 11:22:45 -0300

    Derpartment Form

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -13,4 +13,13 @@ private DepartmentDao dao = DaoFactory.createDepartmentDao();
 	public List<Department> findAll() {
 		return dao.findAll();
 	}
+	
+	public void saveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
 }
